c404350acba1badb45ca63f3bd3c6c03db625115
rnielikki <lumi.kwon@student.lamk.fi>
2021-10-26 14:03:03 +0300

    hatapon, sukopon working

---
+++ Assets/sources/core/character/equipment/weapon/special/Cannon.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Core.Character.Equipment.Weapon
+{
+    class Cannon : WeaponObject
+    {
+        /// <summary>
+        /// An arrow "transform" with animation, before shooting.
+        /// </summary>
+        private Transform _arrowTransform;
+        /// <summary>
+        /// copied arrow for throwing.
+        /// </summary>
+        private GameObject _copiedArrow;
+        private Sprite _arrowSprite;
+        private void Awake()
+        {
+            Init();
+            _arrowTransform = transform.Find("Arrow");
+            _copiedArrow = GetWeaponInstance();
+            _arrowSprite = _arrowTransform.GetComponent<SpriteRenderer>().sprite;
+        }
+        /// <summary>
+        /// Throws spear, from CURRENT spear position and rotation FROM ANIMATION.
+        /// </summary>
+        public override void Attack(AttackCommandType attackCommandType)
+        {
+            var arrowForThrowing = Instantiate(_copiedArrow, transform.root.parent);
+            arrowForThrowing.GetComponent<SpriteRenderer>().sprite = _arrowSprite;
+            arrowForThrowing.transform.position = _arrowTransform.position;
+            arrowForThrowing.transform.rotation = _arrowTransform.rotation;
+            arrowForThrowing.GetComponent<WeaponInstance>().Throw((attackCommandType == AttackCommandType.Defend) ? 1 : 1.5f);
+        }
+    }
+}
