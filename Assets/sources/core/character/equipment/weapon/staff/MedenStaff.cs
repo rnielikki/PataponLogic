4ff6dc3ab5b19600300559a597320d23963e14a0
rnielikki <lumi.kwon@student.lamk.fi>
2021-10-26 20:21:42 +0300

    Added Meden

---
+++ Assets/sources/core/character/equipment/weapon/staff/MedenStaff.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+//Let me think how to create various staffs...
+namespace Core.Character.Equipment.Weapon
+{
+    /// <summary>
+    /// Meden herself doesn't attack, but boosts attack (damage)/defence (defence)/dodge (movement speed) command etc.
+    /// </summary>
+    public class MedenStaff : Staff
+    {
+        private void Awake()
+        {
+            Init();
+        }
+        public override void NormalAttack()
+        {
+        }
+        public override void ChargeAttack()
+        {
+        }
+
+        public override void Defend()
+        {
+        }
+
+    }
+}
