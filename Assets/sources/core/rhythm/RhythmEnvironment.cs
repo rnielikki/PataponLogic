b0ca4a1b933b7165fb1e0c148d50133bbc310ff6
rnielikki <lumi.kwon@student.lamk.fi>
2021-11-06 18:30:46 +0200

    Successfully separated Rhythm assembly

--- Assets/sources/core/rhythm/RhythmEnvironment.cs
+++ Assets/sources/core/rhythm/RhythmEnvironment.cs
@@ -1,24 +1,24 @@
 ﻿namespace PataRoad.Core.Rhythm
 {
-    internal static class RhythmEnvironment
+    public static class RhythmEnvironment
     {
         //Paths
-        internal const string ThemePath = "Rhythm/themes/";
-        internal const string DrumSoundPath = "Rhythm/sounds/drums/";
+        public const string ThemePath = "Rhythm/themes/";
+        public const string DrumSoundPath = "Rhythm/sounds/drums/";
 
         //Rnages and intervals for one drum
-        internal const float UpdateSecond = 0.01f; //"Fixed update" second
-        internal const float InputInterval = 0.5f; //Can be changed for 1/8 beat minigame
-        internal const float HalfInputInterval = InputInterval / 2;
-        internal const float PerfectRange = 0.1f; //Was 0.05f
-        internal const float GoodRange = 0.2f; //Was 0.1f
-        internal const float BadRange = 0.25f;
+        public const float UpdateSecond = 0.01f; //"Fixed update" second
+        public const float InputInterval = 0.5f; //Can be changed for 1/8 beat minigame
+        public const float HalfInputInterval = InputInterval / 2;
+        public const float PerfectRange = 0.1f; //Was 0.05f
+        public const float GoodRange = 0.2f; //Was 0.1f
+        public const float BadRange = 0.25f;
 
         //Command
-        internal const float TurnSeconds = 4 * InputInterval;
+        public const float TurnSeconds = 4 * InputInterval;
 
         //Fever
-        internal const int FeverPerfectRequirement = 4;
-        internal const int FeverMaximum = 10;
+        public const int FeverPerfectRequirement = 4;
+        public const int FeverMaximum = 10;
     }
 }
