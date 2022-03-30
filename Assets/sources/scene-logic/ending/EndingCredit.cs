d054d90443d177c3c627af515d6b54a0b0216693
rnielikki <lumi.kwon@student.lamk.fi>
2022-03-30 18:20:30 +0300

    added ending credit

---
+++ Assets/sources/scene-logic/ending/EndingCredit.cs
@@ -0,0 +1,138 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace PataRoad.SceneLogic.Ending
+{
+    public class EndingCredit : MonoBehaviour
+    {
+        [SerializeField]
+        EndingCreditData[] _data;
+        [SerializeField]
+        TextMeshProUGUI _bigTitle;
+        [SerializeField]
+        TextMeshProUGUI _creditTitle1;
+        [SerializeField]
+        TextMeshProUGUI _credit1;
+        [SerializeField]
+        TextMeshProUGUI _creditTitle2;
+        [SerializeField]
+        TextMeshProUGUI _credit2;
+
+        [SerializeField]
+        float _displaySeconds;
+        [SerializeField]
+        float _fadingSeconds;
+        float _currentSecond;
+        bool _started;
+        bool _fullyDisplayed;
+
+        readonly System.Collections.Generic.List<TextMeshProUGUI> _fadingTarget
+            = new System.Collections.Generic.List<TextMeshProUGUI>();
+
+        private int _currentIndex = -1;
+        private EndingCreditData _currentData => _currentIndex < 0 ? null : _data[_currentIndex];
+        float _currentShowingSeconds;
+
+        private void Start()
+        {
+            _creditTitle1.gameObject.SetActive(false);
+            _creditTitle2.gameObject.SetActive(false);
+            _credit1.gameObject.SetActive(false);
+            _credit2.gameObject.SetActive(false);
+            _bigTitle.gameObject.SetActive(false);
+            DisplayCredit();
+        }
+        private void DisplayCredit()
+        {
+            _started = true;
+        }
+        private void DisplayCurrent()
+        {
+            _fadingTarget.Clear();
+            _currentData.CallDisplayEvent();
+            if (_currentData.UseBigTitle)
+            {
+                SetTextAndAddTarget(_bigTitle, _currentData.BigTitleText);
+            }
+            else
+            {
+                if (!_currentData.HideCredit1)
+                {
+                    SetTextAndAddTarget(_creditTitle1, _currentData.Title1);
+                    SetTextAndAddTarget(_credit1, _currentData.Content1);
+                }
+                if (!_currentData.HideCredit2)
+                {
+                    SetTextAndAddTarget(_creditTitle2, _currentData.Title2);
+                    SetTextAndAddTarget(_credit2, _currentData.Content2);
+                }
+            }
+            Fade(0);
+
+            void SetTextAndAddTarget(TextMeshProUGUI target, string text)
+            {
+                target.gameObject.SetActive(true);
+                target.text = text;
+                _fadingTarget.Add(target);
+            }
+        }
+        private void EndDisplaying()
+        {
+            _started = false;
+        }
+        // coroutine is enemy of sync.
+        void Update()
+        {
+            if (!_started) return;
+            _currentSecond += Time.deltaTime;
+            _currentShowingSeconds += Time.deltaTime;
+            var index = (int)(_currentSecond / _displaySeconds);
+            if (_currentIndex != index)
+            {
+                Hide();
+                _currentShowingSeconds = 0;
+                _currentData?.CallHidingEvent();
+                if (index < _data.Length)
+                {
+                    _currentIndex = index;
+                    DisplayCurrent();
+                }
+                else EndDisplaying();
+            }
+            else if (_currentShowingSeconds < _fadingSeconds)
+            {
+                _fullyDisplayed = false;
+                //fade in
+                Fade(_currentShowingSeconds / _fadingSeconds);
+            }
+            else if (_displaySeconds - _currentShowingSeconds < _fadingSeconds)
+            {
+                _fullyDisplayed = false;
+                //fade out
+                Fade((_displaySeconds - _currentShowingSeconds) / _fadingSeconds);
+            }
+            else if (!_fullyDisplayed)
+            {
+                _fullyDisplayed = true;
+                //keep opacity 1
+                Fade(1);
+            }
+        }
+        private void Fade(float opacity)
+        {
+            foreach (var target in _fadingTarget)
+            {
+                var color = target.color;
+                color.a = opacity;
+                target.color = color;
+            }
+        }
+        private void Hide()
+        {
+            foreach (var target in _fadingTarget)
+            {
+                target.gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/sources/scene-logic/ending/EndingCreditData.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace PataRoad.SceneLogic.Ending
+{
+    [System.Serializable]
+    public class EndingCreditData
+    {
+        [Header("Events")]
+        [SerializeField]
+        UnityEngine.Events.UnityEvent _onDisplay;
+        [SerializeField]
+        UnityEngine.Events.UnityEvent _onHidden;
+        [Header("BigTitle")]
+        [SerializeField]
+        bool _useBigTitle;
+        public bool UseBigTitle => _useBigTitle;
+        [SerializeField]
+        [TextArea]
+        string _bigTitleText;
+        public string BigTitleText => _bigTitleText;
+        [Header("Credit1")]
+        [SerializeField]
+        bool _hide1;
+        public bool HideCredit1 => _hide1;
+        [SerializeField]
+        string _title1;
+        public string Title1 => _title1;
+        [SerializeField]
+        [TextArea]
+        string _content1;
+        public string Content1 => _content1;
+        [Header("Credit2")]
+        [SerializeField]
+        bool _hide2;
+        public bool HideCredit2 => _hide2;
+        [SerializeField]
+        string _title2;
+        public string Title2 => _title2;
+        [SerializeField]
+        [TextArea]
+        string _content2;
+        public string Content2 => _content2;
+        public void CallDisplayEvent() => _onDisplay.Invoke();
+        public void CallHidingEvent() => _onHidden.Invoke();
+    }
+}
