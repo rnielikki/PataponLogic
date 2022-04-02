9e831022bfb32964cdff43fc8ae24e93f9f5509c
rnielikki <lumi.kwon@student.lamk.fi>
2022-04-02 22:18:10 +0300

    lol an useless feature added

---
+++ Assets/sources/scene-logic/patapolis/SequenceInput.cs
@@ -0,0 +1,55 @@
+﻿using PataRoad.Core.Global;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace PataRoad.SceneLogic.Patapolis
+{
+    public class SequenceInput : MonoBehaviour
+    {
+        [SerializeField]
+        string[] _inputActions;
+        InputAction[] _sequence;
+        private int _sequenceIndex;
+        void Start()
+        {
+            _sequence = new InputAction[_inputActions.Length];
+            for (int i = 0; i < _sequence.Length; i++)
+            {
+                _sequence[i] = GlobalData.Input.actions
+                    .FindAction(_inputActions[i]);
+            }
+            _sequence[0].performed += StartListening;
+        }
+        void StartListening(InputAction.CallbackContext context)
+        {
+            var action = context.action;
+            action.performed -= StartListening;
+
+            if (action == _sequence[_sequenceIndex])
+            {
+                _sequenceIndex++;
+                if (_sequenceIndex >= _sequence.Length)
+                {
+                    CallInputAction();
+                }
+                else
+                {
+                    _sequence[_sequenceIndex].performed += StartListening;
+                }
+            }
+            else
+            {
+                _sequenceIndex = 0;
+            }
+        }
+        private void CallInputAction()
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("lol");
+            _sequenceIndex = 0;
+        }
+        private void OnDestroy()
+        {
+            _sequence[_sequenceIndex].performed -= StartListening;
+        }
+    }
+}
